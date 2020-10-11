e663853ecd75b82401fdef3ca750eebdac554656
anubhavajmera <anubhavajmera123@gmail.com>
2020-10-11 16:18:26 +0530

    Final

--- lib/home_screen/components/bottom_sheet.dart
+++ lib/home_screen/components/bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:qc4application/home_screen/models/slider_amount.dart';
+import 'package:qc4application/main.dart';
 
 class BottomSheetScreen extends StatefulWidget {
   @override
@@ -16,88 +17,156 @@ class _BottomSheetScreenState extends State<BottomSheetScreen> {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
-      height: MediaQuery.of(context).size.height * 0.8,
+      height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Select Payment Amount",
-            style: TextStyle(fontSize: 20, color: Colors.black),
-          ),
-          Text(
-            "Move the slider to set up your payment amount",
-            style: TextStyle(fontSize: 12, color: Colors.grey),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Stack(
-            alignment: Alignment.centerLeft,
-            children: <Widget>[
-              Builder(builder: (context) {
-                return Text(
-                  '    Rp    ',
-                  style: TextStyle(color: Colors.black45),
-                );
-              }),
-              TextFormField(
-                keyboardType: TextInputType.number,
-                cursorColor: Colors.grey,
-                onSaved: (newValue) => amount = newValue,
-                controller: amountController,
-                decoration: InputDecoration(
-                  hintText: "120000",
-                  hintStyle: TextStyle(color: Colors.black38),
-                  prefixText: "Rp   ",
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.grey),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Select Payment Amount",
+              style: TextStyle(fontSize: 20, color: Colors.black),
+            ),
+            Text(
+              "Move the slider to set up your payment amount",
+              style: TextStyle(fontSize: 12, color: Colors.grey),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Stack(
+              alignment: Alignment.centerLeft,
+              children: <Widget>[
+                Builder(builder: (context) {
+                  return Text(
+                    '    Rp    ',
+                    style: TextStyle(color: Colors.black45),
+                  );
+                }),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  cursorColor: Colors.grey,
+                  onSaved: (newValue) => amount = newValue,
+                  controller: amountController,
+                  decoration: InputDecoration(
+                    hintText: "120000",
+                    hintStyle: TextStyle(color: Colors.black38),
+                    prefixText: "Rp   ",
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    prefixStyle: TextStyle(color: Colors.transparent),
+                    contentPadding: EdgeInsets.only(left: 10),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
                   ),
-                  prefixStyle: TextStyle(color: Colors.transparent),
-                  contentPadding: EdgeInsets.only(left: 10),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Slider(
-            value: val,
-            min: 0.0,
-            max: 10000.0,
-            onChanged: (double value) {
-              setState(() {
-                val = value;
-              });
-              var sliderAmount =
-                  Provider.of<SliderAmount>(context, listen: false);
-              sliderAmount.updateAmount(value);
-            },
-            activeColor: Colors.deepPurpleAccent,
-            inactiveColor: Colors.grey[500],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text("My Balance", style: TextStyle(color: Colors.grey),),
-              Consumer<SliderAmount>(
-                builder: (context, data, child){
-                  var value = data.getAmount();
-                  var valShort = double.parse(value.toStringAsFixed(3));
-                  return Text("Rp "+ valShort.toString(), style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),);
-                },
-              )
-            ],
-          )
-        ],
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Slider(
+              value: val,
+              min: 0.0,
+              max: 10000.0,
+              onChanged: (double value) {
+                setState(() {
+                  val = value;
+                });
+                var sliderAmount =
+                    Provider.of<SliderAmount>(context, listen: false);
+                sliderAmount.updateAmount(value);
+              },
+              activeColor: Colors.deepPurpleAccent,
+              inactiveColor: Colors.grey[500],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("My Balance", style: TextStyle(color: Colors.grey),),
+                Consumer<SliderAmount>(
+                  builder: (context, data, child){
+                    var value = data.getAmount();
+                    var valShort = double.parse(value.toStringAsFixed(3));
+                    return Text("Rp "+ valShort.toString(), style: TextStyle(color: Colors.black, fontSize: 14, fontWeight: FontWeight.bold),);
+                  },
+                )
+              ],
+            ),
+            SizedBox(height: 15,),
+            Text("Select Category", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+            SizedBox(height: 10,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CategoryItem(color: Colors.deepPurpleAccent[100], icon: Icons.car_repair,),
+                CategoryItem(color: Colors.grey[400], icon: Icons.car_repair,),
+                CategoryItem(color: Colors.grey[400], icon: Icons.car_repair,),
+                CategoryItem(color: Colors.grey[400], icon: Icons.car_repair,)
+              ],
+            ),
+            Text("Descrition", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+            Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FlatButton(
+                  onPressed: (){
+                    Navigator.of(context).pop();
+                  },
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(15)),
+                      border: Border.all(color: Colors.green, width: 1)
+                    ),
+                    child: Center(child: Text("CANCEL", style: TextStyle(fontSize: 12),)),
+                  )
+                ),
+                FlatButton(
+                    onPressed: (){
+
+                    },
+                    child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                          borderRadius: BorderRadius.all(Radius.circular(15)),
+                          border: Border.all(color: Colors.green, width: 1)
+                      ),
+                      child: Center(child: Text("CREATE PAYMENT", style: TextStyle(fontSize: 12, color: Colors.white),)),
+                    )
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+class CategoryItem extends StatelessWidget {
+  final IconData icon;
+  final Color color;
+  const CategoryItem({Key key, this.icon, this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(6),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: color
+      ),
+      child: Icon(icon, size: 35,),
+    );
+  }
+}
+
--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -10,27 +10,48 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   int _currentIndex = 0;
 
+  List<String> categoryList = ["All", "Food", "Shopping", "Rent"];
+  List<Map> expenseList = [
+    {
+      "icon": Icons.car_repair,
+      "title": "Coffee Latte",
+      "subtitle": "Today, 10:00 am",
+      "amount": "-Rp25.000",
+      "color": Colors.redAccent,
+    },
+    {
+      "icon": Icons.car_repair,
+      "title": "Online Taxi",
+      "subtitle": "Today, 10:00 am",
+      "amount": "-Rp25.000",
+      "color": Colors.greenAccent,
+    },
+    {
+      "icon": Icons.car_repair,
+      "title": "Lunch",
+      "subtitle": "Today, 10:00 am",
+      "amount": "-Rp25.000",
+      "color": Colors.redAccent,
+    },
+    {
+      "icon": Icons.car_repair,
+      "title": "Bank Transfer",
+      "subtitle": "Today, 10:00 am",
+      "amount": "-Rp25.000",
+      "color": Colors.redAccent,
+    }
+  ];
   List<BottomNavigationBarItem> buildBottomNavBarItems() {
     return [
+      BottomNavigationBarItem(icon: Icon(Icons.home), title: Text("Home")),
       BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          title: Text("Home")
-      ),
+          icon: Icon(Icons.pie_chart), title: Text("Graphics")),
       BottomNavigationBarItem(
-          icon: Icon(Icons.pie_chart),
-          title: Text("Graphics")
-      ),
+          icon: Icon(Icons.add_alert), title: Text("Alert")),
       BottomNavigationBarItem(
-          icon: Icon(Icons.add_alert),
-          title: Text("Alert")
-      ),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          title: Text("Settings")
-      ),
+          icon: Icon(Icons.settings), title: Text("Settings")),
     ];
   }
 
@@ -43,60 +64,129 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-          children: [
-            SizedBox(
-                height: MediaQuery.of(context).size.height * 0.35,
-                width: MediaQuery.of(context).size.width,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(bottomLeft: Radius.circular(70), bottomRight: Radius.circular(70)),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 50),
-                    color: Colors.deepPurpleAccent,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: MediaQuery.of(context).size.width,
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(70),
+                  bottomRight: Radius.circular(70)),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 50),
+                color: Colors.deepPurpleAccent,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Column(
                           children: [
-                            Column(
-                              children: [
-                                Text("Total Balance", style: TextStyle(color: Colors.white, fontSize: 12),),
-                                SizedBox(height: 5,),
-                                Consumer<SliderAmount>(
-                                  builder: (context, data, child){
-                                    var value = data.getAmount();
-                                    var valShort = double.parse(value.toStringAsFixed(3));
-                                    return Text("Rp "+ valShort.toString(), style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),);
-                                  },
-                                )
-                              ],
+                            Text(
+                              "Total Balance",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 12),
                             ),
-                            Spacer(),
-                            CircleAvatar(
-                              child: Icon(Icons.person),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Consumer<SliderAmount>(
+                              builder: (context, data, child) {
+                                var value = data.getAmount();
+                                var valShort =
+                                    double.parse(value.toStringAsFixed(3));
+                                return Text(
+                                  "Rp " + valShort.toString(),
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                );
+                              },
                             )
                           ],
                         ),
-                        SizedBox(height: 20,),
-                        Row(
-                          children: [
-                            ProfileCard(text: "Monthly Income", amount: "200.000",),
-                            Spacer(),
-                            ProfileCard(text: "Monthly Spendings", amount: "200.000",)
-                          ],
+                        Spacer(),
+                        CircleAvatar(
+                          child: Icon(Icons.person),
                         )
-                        //bottomContent,
                       ],
                     ),
-                  ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        ProfileCard(
+                          text: "Monthly Income",
+                          amount: "200.000",
+                        ),
+                        Spacer(),
+                        ProfileCard(
+                          text: "Monthly Spendings",
+                          amount: "200.000",
+                        )
+                      ],
+                    )
+                    //bottomContent,
+                  ],
                 ),
+              ),
             ),
-            Padding(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.318, left: MediaQuery.of(context).size.width * 0.15),
-              child: IconBar(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.318,
+                left: MediaQuery.of(context).size.width * 0.15),
+            child: IconBar(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.41,
+                left: MediaQuery.of(context).size.width * 0.08),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Today's Transaction",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  child: ListView.builder(
+                      itemCount: categoryList.length,
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Chip(
+                            label: Text(categoryList[index]),
+                          ),
+                        );
+                      }),
+                ),
+                Container(
+                  padding: EdgeInsets.only(right: MediaQuery.of(context).size.width * 0.08),
+                  height: MediaQuery.of(context).size.height * 0.35,
+                  child: Card(
+                    child: ListView.builder(
+                      itemCount: expenseList.length,
+                      itemBuilder: (context, index){
+                        return ExpensesItem(icon: expenseList[index]["icon"], title: expenseList[index]["title"], subtitle: expenseList[index]["subtitle"], color: expenseList[index]["color"], amount: expenseList[index]["amount"],);
+                      }
+                    ),
+                  ),
+                )
+              ],
             ),
-          ],
-        ),
+          )
+        ],
+      ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex,
@@ -114,3 +204,40 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+class ExpensesItem extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String title, subtitle, amount;
+  const ExpensesItem({Key key, this.color, this.icon, this.title, this.subtitle, this.amount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(
+          horizontal: 15, vertical: 5),
+      decoration: BoxDecoration(
+          border: Border(
+              bottom: BorderSide(
+                  width: 1, color: Colors.grey))),
+      child: ListTile(
+        leading: Container(
+          padding: EdgeInsets.all(6),
+          decoration: BoxDecoration(
+              borderRadius:
+              BorderRadius.all(Radius.circular(10)),
+              color: color),
+          child: Icon(
+            icon,
+            size: 40,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(title, style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+        subtitle: Text(subtitle),
+        trailing: Text(amount, style: TextStyle(color: color),),
+      ),
+    );
+  }
+}
+
